3fb47adeaf1ed8cf328bbba5ede12007f15b24bd
Rayanne Bertolace Lima <rayanne_lima2010@hotmail.com>
2022-06-11 12:18:10 -0300

    fix: update method

--- src/composables/useAPI.js
+++ src/composables/useAPI.js
@@ -41,9 +41,9 @@ export default function useAPI () {
   const update = async (table, form) => {
     const { data, error } = await supabase
       .from(table)
-      .update([
+      .update({
         ...form
-      ])
+      })
       .match({ id: form.id })
 
     if (error) throw error
