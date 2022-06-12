dd252ba252d88795eac19ae8bda060f5489901b9
Rayanne Bertolace Lima <rayanne_lima2010@hotmail.com>
2022-06-12 18:50:03 -0300

    fix: column created_at removed from view

--- src/pages/product/table.js
+++ src/pages/product/table.js
@@ -4,8 +4,7 @@ const productColumns = [
   { name: 'name', align: 'center', label: 'Nome', field: 'name', sortable: true },
   { name: 'amount', align: 'center', label: 'Quantidade', field: 'amount', sortable: true },
   { name: 'price', align: 'center', label: 'Preço', field: 'price', sortable: true },
-  { name: 'category', align: 'center', label: 'Categoria', field: 'category_id', sortable: true },
-  { name: 'created_at', align: 'center', label: 'Criado em', field: 'created_at' },
+  { name: 'category_name', align: 'center', label: 'Categoria', field: 'category_name', sortable: true },
   { name: 'actions', align: 'center', label: 'Ações', field: 'actions' }
 ]
 
