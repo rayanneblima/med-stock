41e2f94c05074be7276ddc0a9fd4d623d8556e93
Rayanne Bertolace Lima <rayanne_lima2010@hotmail.com>
2022-07-02 10:22:05 -0300

    fix: responsive color picker

--- src/pages/StoreConfigPage.vue
+++ src/pages/StoreConfigPage.vue
@@ -62,11 +62,11 @@
         </q-file>
 
         <div class="row justify-center q-gutter-md q-pa-md">
-          <q-field borderless class="col-xs-12 col-sm-5" label="Cor Principal">
-            <q-color v-model="form.primary_color" class="q-mt-md" />
+          <q-field borderless class="col-xs-12 col-sm-5 col-lg-3" label="Cor Principal">
+            <q-color v-model="form.primary_color" class="full-width q-mt-md" />
           </q-field>
-          <q-field borderless class="col-xs-12 col-sm-5" label="Cor Secundária">
-            <q-color v-model="form.secondary_color" class="q-mt-md" />
+          <q-field borderless class="col-xs-12 col-sm-5 col-lg-3" label="Cor Secundária">
+            <q-color v-model="form.secondary_color" class="full-width q-mt-md" />
           </q-field>
         </div>
 
