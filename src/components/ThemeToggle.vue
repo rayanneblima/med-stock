bfe09bf8d089d3994f6d4a263dbb138251af1201
Rayanne Bertolace Lima <rayanne_lima2010@hotmail.com>
2022-07-04 17:49:19 -0300

    add: toggle dark mode

---
+++ src/components/ThemeToggle.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-toggle
+    v-model="isDarkMode"
+    color="accent"
+    checked-icon="mdi-weather-night"
+    unchecked-icon="mdi-white-balance-sunny"
+    size="lg"
+  />
+</template>
+
+<script>
+import { defineComponent, onMounted, ref, watch } from '@vue/runtime-core'
+import { useQuasar } from 'quasar'
+
+export default defineComponent({
+  name: 'ThemeToggle',
+
+  setup () {
+    const isDarkMode = ref(false)
+    const $q = useQuasar()
+
+    watch(isDarkMode, (isDarkMode) => {
+      $q.dark.set(isDarkMode)
+      $q.localStorage.set('dark-mode', isDarkMode)
+    })
+
+    onMounted(() => {
+      isDarkMode.value = $q.localStorage.getItem('dark-mode')
+    })
+
+    return {
+      isDarkMode
+    }
+  }
+})
+</script>
