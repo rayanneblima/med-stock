5cb46fbb2d443d85f1f5485f05ca3c76cd3de790
Rayanne Bertolace Lima <rayanne_lima2010@hotmail.com>
2022-07-04 12:04:38 -0300

    feature: icongenie

--- quasar.config.js
+++ quasar.config.js
@@ -178,7 +178,7 @@ module.exports = configure(function (ctx) {
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#027be3',
+        theme_color: '#0e7ba8',
         icons: [
           {
             src: 'icons/icon-128x128.png',
